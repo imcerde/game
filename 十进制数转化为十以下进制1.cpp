fdeb4c69eefb7fa9c969e1fe52bbee6390f17020
imcerde <32219000+imcerde@users.noreply.github.com>
2017-09-26 08:19:45 +0800

    Add files via upload

---
+++ 十进制数转化为十以下进制1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b,c;
+	int f=0;
+    int	i=0;
+	printf("please insert two munbers£º");
+	scanf("%d%d",&a,&b);
+	while(a>b)
+	{
+		c=a%b;
+		a=a/b;
+		for(int j=0;j<i;j++)
+		{
+			c*=10;
+		}
+		f+=c;
+		i++;	
+	}
+	for(int j=0;j<i;j++){
+		a*=10;
+	} 
+	f+=a;
+	printf("%d",f);
+	return 0;
+}	
